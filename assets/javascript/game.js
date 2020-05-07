ac4b0fd9f9acc63bd4412af257475b337a74d3c8
Alex <alexander.alea@hotmail.com>
2020-05-07 14:24:27 -0500

    assets/javascript/game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,22 @@
+//team one//
 
 let shotsOne = document.querySelector("#teamone-numshots")
 let goalsOne = document.querySelector("#teamone-numgoals")
-
 let teamOneShootButton = document.querySelector("#teamone-shoot-button")
+
+//team two//
 
 let shotsTwo = document.querySelector("#teamtwo-numshots")
 let goalsTwo = document.querySelector("#teamtwo-numgoals")
-
 let teamTwoShootButton = document.querySelector("#teamtwo-shoot-button")
 
-//team one
+
+//Reset Function//
+
+let resetButto = document.querySelector("#reset-button")
+
+
+//team one shots and goals//
 
 teamOneShootButton.addEventListener("click", function() {
 
@@ -17,15 +24,51 @@ teamOneShootButton.addEventListener("click", function() {
     let teamOneNumShots = Number(shotsOne.innerHTML) + 1;
     shotsOne.innerHTML = teamOneNumShots
 
+    teamOneNumGoals = Number (goalsOne.innerHTML) + 1;
+
+    if (Math.random()>= 0.5 ) {
+        goalsOne.innerHTML = teamOneNumGoals;
+    }
+
+    else {
+
+    }
 })
 
-
-//team two
+//team two shots and goals//
 
 teamTwoShootButton.addEventListener("click", function() {
 
     console.log("teamTwoShootButton button clicked");
     let teamTwoNumShots = Number(shotsTwo.innerHTML) + 1;
     shotsTwo.innerHTML = teamTwoNumShots
+
+    teamTwoNumGoals = Number (goalsTwo.innerHTML) + 1;
+
+    if (Math.random()>= 0.5 ) {
+        goalsTwo.innerHTML = teamTwoNumGoals;
+    }
+
+    else {
+
+    }
+})
+
+
+//Reset//
+
+resetButton.addEventListener("click", function() {
+
+console.log("Reset button clicked");
+let resetFunction = Number(resetButton.innerHTML) + 1;
+
+    if {
+
+}
+
+    else {
+
+    }
+
 
 })
